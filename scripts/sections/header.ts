246a8515abe084a7ff11e5b81b0002d65323051b
unknown <ashokkumar.sm@SON15652.SSLTUIODC.LOCAL>
2013-11-25 17:35:39 +0530

    Loggin text in home, and changes in home.scss

--- scripts/sections/header.ts
+++ scripts/sections/header.ts
@@ -1,7 +1,10 @@
 $('./body'){
 	$("./div[@id='wrapper']"){
 		$(".//div[@id='header']"){
-			
+			log("logging some content here")
+		}
+		$(".//div[@class='content']"){
+			log("loggin more")
 		}
 	}
 }	
