532037fe61ce363eb108c7ed24bc1f0ed022f53f
unknown <siddhant.w@BG1WS0089.SONATA.LOCAL>
2013-11-25 15:30:48 +0530

    responsivebody n footer

--- scripts/sections/footer.ts
+++ scripts/sections/footer.ts
@@ -1,12 +1,24 @@
 $('./body'){
 	$("./div[@id='wrapper']"){
 		$("./div[@id='footer']"){
-			$(".//div[contains(@class,'col-4')]"){
-				move_here("../../../../../.././/ul[@class='social']","top")
-			}
-			$(".//div[@class='container']"){
-				$(".//div[@id='footermenu3']"){
+			$(".//div[@class='container-holder']"){
+			
+				move_here("./div[@id='footermenu2']","top"){
+					$("./ul[contains(@class,'country-list')]"){
+						$(".//ul"){
+							insert_after("div",id:"hrdottedLine")
+						}
+					}
+				}
+				
+				$("./div[@id='footermenu3']"){
 					remove('./p')
+				}
+				
+				$("./div[contains(@class,'col-4')]"){
+					move_here("../../../../../.././/ul[@class='social']","top"){
+						remove(".//li[@class='livechat']")
+					}
 				}
 			
 			}
