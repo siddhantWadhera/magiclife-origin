3c58eaa44f45a9293cc5a0191227818f242d594e
unknown <siddhant.w@BG1WS0089.SONATA.LOCAL>
2013-12-02 11:32:57 +0530

    clublist

--- scripts/sections/footer.ts
+++ scripts/sections/footer.ts
@@ -2,6 +2,7 @@ $('./body'){
 	$("./div[@id='wrapper']"){
 		$("./div[@id='footer']"){
 			$(".//div[@class='container-holder']"){
+			
 				move_here("./div[@id='footermenu2']","top"){
 					$("./ul[contains(@class,'country-list')]"){
 						$(".//ul"){
@@ -15,7 +16,9 @@ $('./body'){
 				}
 				
 				$("./div[contains(@class,'col-4')]"){
+					#move_here("../../../../../.././/ul[@class='social']","top")
 					move_here("./ancestor::ul[@class='social']","top"){
+						log('-------------------------->')
 						remove(".//li[@class='livechat']")
 					}
 				}
